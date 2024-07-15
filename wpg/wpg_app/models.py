5187cf051386c1a9522463976e59d0e4d692a8fb
Jules NTIHEMUKA <julesntihemuka@gmail.com>
2024-07-15 17:52:42 +0200

    first init

--- wpg/wpg_app/models.py
+++ wpg/wpg_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
-# Create your models here.
+class ProjectItem(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    ProjectImage = models.ImageField(upload_to="images/")
+    def __str__(self):
+        return self.title +   ' ' + self.description
