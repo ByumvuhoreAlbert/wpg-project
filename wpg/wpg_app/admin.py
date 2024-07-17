de7443f5d3be23dde2c742b41734fd81f5ff8b1c
Jules75Nmk <julesntihemuka@gmail.com>
2024-07-17 08:21:40 +0200

    project commited

--- wpg/wpg_app/admin.py
+++ wpg/wpg_app/admin.py
@@ -1,4 +1,18 @@
-from django.contrib import admin
-from .models import ProjectItem
+# from django.contrib import admin
+#from .models import ProjectItem
 
-admin.site.register(ProjectItem)
+#admin.site.register(ProjectItem)
+# admin.py
+from django.contrib import admin
+#from .models import Proj
+#from .models import Project,proj, project_table
+from .models import Project,project_table
+class Project_Table_Admin(admin.ModelAdmin):
+ list_display = ('title', 'height', 'width', 'date_published', 'amount')
+admin.site.register( project_table)
+admin.site.register(Project)
+
+
+
+
+#admin.site.register(Proj)
---
+++ wpg/wpg_app/forms.py
@@ -0,0 +1,7 @@
+# from django import forms
+# from .models import Proj
+
+# class ProjectForm(forms.ModelForm):
+#     class Meta:
+#         model = Proj
+#         fields = ['title', 'height', 'width', 'date_published', 'amount', 'image']
