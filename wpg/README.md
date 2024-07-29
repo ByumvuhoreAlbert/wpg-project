72a1eba424533112f1c8ce74394417534d4e204a
ByumvuhoreAlbert <byumvualbert1994@gmail.com>
2024-07-29 13:20:12 +0200

    updated files

--- wpg/README.md
+++
@@ -1,2 +0,0 @@
-# wpg-project
-this is the website for advertising the wisdom palace group services and daily working life.
--- wpg/wpg_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class WpgAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'wpg_app'
